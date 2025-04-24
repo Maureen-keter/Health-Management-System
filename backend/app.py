ecd212daa80d46e9d31525cffd4c31f2d34a833c
maureen <maureen.chelangat@student.moringaschool.com>
2025-04-24 17:59:17 +0300

    configure sqlite database and initialize app

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from models import db
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 app= Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db.init_app(app)
 migrate=Migrate(app,db)
 
 
