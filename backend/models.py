343c410ce91b7bb35d5556f6e2828d5c5a9b7250
maureen <maureen.chelangat@student.moringaschool.com>
2025-04-24 16:44:39 +0300

    create a users model

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+
 
 
 db=SQLAlchemy()
+
+class User(db.Model, SerializerMixin):
+    __tablename__='users'
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(100) required=True)
+    email=db.Column(db.String(100) unique=True)
+    password=db.Column(db.String(50))
