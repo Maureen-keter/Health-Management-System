2b694b7d9d215d152c9cc86eedcbdce416ce89f4
maureen <maureen.chelangat@student.moringaschool.com>
2025-04-24 16:55:02 +0300

    add comma on name and email

--- backend/models.py
+++ backend/models.py
@@ -10,8 +10,8 @@ db=SQLAlchemy()
 class User(db.Model, SerializerMixin):
     __tablename__='users'
     id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(100) required=True)
-    email=db.Column(db.String(100) unique=True)
+    name=db.Column(db.String(100), required=True)
+    email=db.Column(db.String(100), unique=True)
     contact=db.Column(db.Integer(50))
     password=db.Column(db.String(50))
 
