fca6064d2692d7f351f6d184cbf0d5f16dc65b0a
maureen <maureen.chelangat@student.moringaschool.com>
2025-04-24 18:05:05 +0300

    remove the integer number

--- backend/models.py
+++ backend/models.py
@@ -13,7 +13,7 @@ class User(db.Model, SerializerMixin):
     id=db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String(100), nullable=False)
     email=db.Column(db.String(100), unique=True)
-    contact=db.Column(db.Integer(50))
+    contact=db.Column(db.Integer)
     password=db.Column(db.String(50))
 
     enrollments=db.relationship("Enrollment", backref="user", lazy=True)
