d9e2c7b3bf8a13c9baf2b87faa26a491742137cd
maureen <maureen.chelangat@student.moringaschool.com>
2025-04-25 06:54:35 +0300

    create a route to post a program

--- backend/controllers/enrollments.py
+++ backend/controllers/enrollments.py
@@ -7,4 +7,10 @@ class Enrollments(Resource):
         enrollments_dict = [enrollment.to_dict() for enrollment in Enrollment.query.all()]
         return make_response(jsonify(enrollments_dict), 200) 
 
-    
+    def post(self):
+        data = request.get_json()
+        new_enrollment = Enrollment(created_at=data['created_at'], user_id=data['user_id'], program_id=data['program_id'])
+        db.session.add(new_enrollment)
+        db.session.commit()
+
+        return make_response(jsonify(new_enrollment.to_dict()), 201)
