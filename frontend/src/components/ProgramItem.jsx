6ea27acb6eec7ee25c074f51f2fa81ab7367b35b
maureen <maureen.chelangat@student.moringaschool.com>
2025-04-25 19:39:12 +0300

    pass in props from ProgramList and return the program item

--- frontend/src/components/ProgramItem.jsx
+++ frontend/src/components/ProgramItem.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-function ProgramItem() {
+function ProgramItem({name, description, onDelete, onEdit}) {
   return (
-    <div>ProgramItem</div>
+    <div>
+        <h2 className="program-name">{name}</h2>
+        <p className="decription">{description}</p>
+        <button onClick={onEdit}>EDIT</button>
+        <button onClick={onDelete}>DELETE</button>
+    </div>
   )
 }
 
