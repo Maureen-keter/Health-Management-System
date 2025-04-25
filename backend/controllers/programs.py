d00c80e3c0c9c54f1dbfe9518d5c37e3e74dabac
maureen <maureen.chelangat@student.moringaschool.com>
2025-04-25 06:50:40 +0300

    create a route to get all programs

--- backend/controllers/programs.py
+++ backend/controllers/programs.py
@@ -7,4 +7,10 @@ class Programs(Resource):
         programs_dict = [program.to_dict() for program in Program.query.all()]
         return make_response(jsonify(programs_dict), 200)  
 
-    
+    def post(self):
+        data = request.get_json()
+        new_program = Program(name=data['name'], description=data['description'])
+        db.session.add(new_program)
+        db.session.commit()
+
+        return make_response(jsonify(new_program.to_dict()), 201)
