8f792458b1aaf3e10884bd115b1e5a2c2a39ac99
maureen <maureen.chelangat@student.moringaschool.com>
2025-04-25 06:51:53 +0300

    create a route to get a single program

--- backend/controllers/programs.py
+++ backend/controllers/programs.py
@@ -14,3 +14,26 @@ class Programs(Resource):
         db.session.commit()
 
         return make_response(jsonify(new_program.to_dict()), 201)
+
+class ProgramById(Resource):
+    def get(self, id):
+        program = Program.query.filter_by(id=id).first()
+        if not program:
+            abort(404, detail="program not found")
+
+        return make_response(jsonify(program.to_dict()), 200)
+
+    def patch(self,id):
+        program = Program.query.filter_by(id=id).first()
+        if not program:
+            abort(404, detail=f'program with {id=} does not exist')
+        data=request.get_json()
+        for key, value in data.items():
+            if value is None:
+                continue
+            setattr(program, key, value)
+        db.session.commit()
+        return make_response(jsonify(program.to_dict()), 200)
+
+
+    
