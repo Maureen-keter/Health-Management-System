e027d7e855d2789ab311be372b2ed0e088cd9529
maureen <maureen.chelangat@student.moringaschool.com>
2025-04-26 11:13:27 +0300

    inititalize state

--- frontend/src/components/ClientList.jsx
+++ frontend/src/components/ClientList.jsx
@@ -3,6 +3,14 @@ import { BASE_URL } from "../api";
 
 function ClientList({ clients, onUpdateClient, onDeleteClient, onClientClick }) {
   
+  const [searchEmail, setSearchEmail] = useState("");
+  const [editingClientId, setEditingClientId] = useState(null);
+  const [editFormData, setEditFormData] = useState({
+    name: "",
+    email: "",
+    contact: "",
+    password: "",
+  });
 
   return (
     <div className="client-list-container">
