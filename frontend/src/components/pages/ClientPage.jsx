7b69c44e19fc1c8868b0c3af91aa6c94c362ef81
maureen <maureen.chelangat@student.moringaschool.com>
2025-04-26 10:58:02 +0300

    set state to hold clients data

---
+++ frontend/src/components/pages/ClientPage.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+
+function ClientPage() {
+  const [clients, setClients] = useState([]); 
+
+
+
+  return (
+    <div className="client-page">
+      <h1>Client Management</h1>
+    </div>
+      
+      )
+}
+
+export default ClientPage;
