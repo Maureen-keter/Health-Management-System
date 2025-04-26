78b1802d90692483b8dbfabe29d3ab6b089db64b
maureen <maureen.chelangat@student.moringaschool.com>
2025-04-26 10:59:28 +0300

    fetch clients data using use state and update state

--- frontend/src/components/pages/ClientPage.jsx
+++ frontend/src/components/pages/ClientPage.jsx
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from "react";
+import { BASE_URL } from '../../api';
 
 
 function ClientPage() {
   const [clients, setClients] = useState([]); 
+
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/users`)
+      .then((res) => res.json())
+      .then((data) => setClients(data)) 
+      .catch((err) => console.error("Error fetching clients:", err));
+  }, []); 
 
 
 
