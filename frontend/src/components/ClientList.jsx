19fa368b5daa89861ee1bff718066578c7eeeeb1
maureen <maureen.chelangat@student.moringaschool.com>
2025-04-26 11:12:41 +0300

    pass props from to client list

---
+++ frontend/src/components/ClientList.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { BASE_URL } from "../api";
+
+function ClientList({ clients, onUpdateClient, onDeleteClient, onClientClick }) {
+  
+
+  return (
+    <div className="client-list-container">
+      
+      </div>
+
+      
+  );
+}
+
+export default ClientList;
